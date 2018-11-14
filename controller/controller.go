a3d8e7407edee80a43d48820292d504822860341
CookiesChen <1109349604@qq.com>
2018-11-14 11:18:02 +0800

    statics

--- controller/controller.go
+++ controller/controller.go
@@ -2,10 +2,22 @@ package controller
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-
 func HomeHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Welcome to the home page!")
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("./public/html/index.html")
+		log.Println(t.Execute(w, nil))
+	}
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request)  {
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/CookiesChen/cloudgo-io/controller"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 var R *mux.Router
@@ -10,5 +11,8 @@ var R *mux.Router
 func init()  {
 	R = mux.NewRouter()
 
+
 	R.HandleFunc("/", controller.HomeHandler)
+	R.HandleFunc("/form", controller.FormHandler)
+	R.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 }
